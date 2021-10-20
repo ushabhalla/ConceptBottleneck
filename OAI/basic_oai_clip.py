c9516ebfe6dc61e1efef55cdf4a8f8d045c42091
ushabhalla <usha_bhalla@brown.edu>
2021-10-20 11:11:57 -0400

    y train

--- OAI/basic_oai_clip.py
+++ OAI/basic_oai_clip.py
@@ -3,7 +3,7 @@ import clip
 import torch
 
 import numpy as np
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression, LogisticRegression
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 from torch.autograd import Variable
@@ -56,14 +56,16 @@ model, preprocess = clip.load('RN50', device)
 # Load the dataset
 # root = os.path.expanduser("~/.cache")
 C_cols=['xrosfm', 'xrscfm', 'xrjsm', 'xrostm', 'xrsctm', 'xrosfl', 'xrscfl', 'xrjsl', 'xrostl', 'xrsctl']
-dataloaders, datasets, dataset_sizes = load_data_from_different_splits(batch_size=1, C_cols=C_cols, y_cols=['xrkl'], zscore_C=True, zscore_Y=False, data_proportion=1.0,
+y_cols=['xrkl']
+dataloaders, datasets, dataset_sizes = load_data_from_different_splits(batch_size=1, C_cols=C_cols, y_cols=y_cols, zscore_C=True, zscore_Y=False, data_proportion=1.0,
     shuffle_Cs=False, merge_klg_01=True, max_horizontal_translation=0.1, max_vertical_translation=0.1, sampling_strategy='uniform', augment='random_translation')
 transform = transforms.ToPILImage(mode='RGB')
 
 
-concept_class_dict = {}
-for c in C_cols:
-    concept_class_dict[c] = LinearRegression()
+# concept_class_dict = {}
+# for c in C_cols:
+#     concept_class_dict[c] = LinearRegression()
+classifier = LogisticRegression(random_state=0, C=0.316, max_iter=1000, verbose=1)
 
 # pdb.set_trace()
 for epoch in range(100):
@@ -77,21 +79,23 @@ for epoch in range(100):
 
                 inputs = preprocess(transform(data_dict['inputs']['image'].squeeze()))
                 inputs = inputs.unsqueeze(0)
-                labels = data_dict['labels']['C_feats']
+                # labels = data_dict['labels']['C_feats']
+                y_labels = data_dict['labels']['y']
 
                 features = model.encode_image(inputs.cuda())
 
                 all_features.append(features.squeeze().cpu().numpy())
-                all_labels.append(labels.squeeze().cpu().numpy())
+                all_labels.append(y_labels.squeeze().cpu().numpy())
             
             all_features = np.array(all_features)
             all_labels = np.array(all_labels)
 
             # Perform logistic regression
             if phase == 'train':
-                for i, c in enumerate(C_cols):
-                    classifier = concept_class_dict[c]
-                    classifier.fit(all_features, all_labels[:,i])
+                classifier.fit(all_features, all_labels)
+                # for i, c in enumerate(C_cols):
+                #     classifier = concept_class_dict[c]
+                #     classifier.fit(all_features, all_labels[:,i])
 
                 # classifier = LogisticRegression(random_state=0, C=0.316, max_iter=1000, verbose=1)
                 # classifier.fit(all_features, all_labels)
@@ -99,10 +103,10 @@ for epoch in range(100):
             # Evaluate using the logistic regression classifier
             else:
                 scores = []
-                for i, c in enumerate(C_cols):
-                    classifier = concept_class_dict[c]
-                    # predictions = classifier.predict(all_features)
-                    score = classifier.score(all_features, all_labels[:,i])
-                    scores.append(score)
-                    # loss = np.mean(all_labels[:,i] - predictions).astype(np.float) 
+                scores.append(classifier.score(all_features, all_labels))
+                # for i, c in enumerate(C_cols):
+                #     classifier = concept_class_dict[c]
+                #     score = classifier.score(all_features, all_labels[:,i])
+                #     scores.append(score)
+
                 print("Epoch:", str(epoch)+ ",", "Scores:", scores)
